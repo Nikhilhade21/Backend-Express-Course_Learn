cf7c6aa2947d372fcfdd519008403e477650784a
Nikhil Hade <nikhilhade21@gmail.com>
2024-03-12 17:51:48 +0530

    custome api response

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is runing at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`MONGODB db connection failed !!!, err`)
+})
 
 
 
